4a2d7e35c391d8705f12c25696fbe3e3a840c0bd
olav <olavgjerde@yahoo.no>
2015-07-02 10:28:41 +0200

    added http status

--- grails-app/controllers/modelandviewerror/AdministrationInterceptor.groovy
+++ grails-app/controllers/modelandviewerror/AdministrationInterceptor.groovy
@@ -10,7 +10,7 @@ class AdministrationInterceptor {
 
     boolean before() {
         log.debug "running administration interceptor before()"
-        render view: '/notFound'
+        render view: '/notFound', status: NOT_FOUND
         return false
     }
 
